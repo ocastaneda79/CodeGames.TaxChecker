6b2dae71f77afcb9f3cf9e1279d533ae98118a55
Oscar castañeda <ocastaneda@wke.es>
2018-04-30 18:31:01 +0200

    new handler

---
+++ Master/InoviceRecievedHandler.cs
@@ -0,0 +1,20 @@
+﻿using NServiceBus;
+using NServiceBus.Logging;
+using Shared.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Master
+{
+    public class InoviceRecievedHandler : IHandleMessages<InvoiceRecievedMessage>
+    {
+        static ILog log = LogManager.GetLogger<InoviceRecievedHandler>();
+        public Task Handle(InvoiceRecievedMessage message, IMessageHandlerContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
